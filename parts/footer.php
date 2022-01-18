95350c46c516caf09ae0a4a579156838c616b2a5
E.Borunov <borunow.e@gmail.com>
2022-01-18 19:45:01 +0300

    Свёрстаны стилизованы разделы в footer.php

--- parts/footer.php
+++ parts/footer.php
@@ -1,5 +1,67 @@
+<footer>
+    <div class="footer-wrap">
+        <div class="chapter-wrap">
+            <div class="chapter">
+                <h4 class="chapter-title">
+                    Коллекции
+                </h4>
+                <a href="#" class="chapter-link">
+                    Женщинам (1725)
+                </a><br>
+                <a href="#" class="chapter-link">
+                    Мужчинам (635)
+                </a><br>
+                <a href="#" class="chapter-link">
+                    Детям (2514)
+                </a><br>
+                <a href="#" class="chapter-link">
+                    Новинки (76)
+                </a>
+            </div>
+        </div>
+        <div class="chapter-wrap col-line">
+            <div class="chapter">
+                <h4 class="chapter-title">
+                    Магазин
+                </h4>
+                <a href="#" class="chapter-link">
+                    О нас
+                </a><br>
+                <a href="#" class="chapter-link">
+                    Доставка
+                </a><br>
+                <a href="#" class="chapter-link">
+                    Работай с нами
+                </a><br>
+                <a href="#" class="chapter-link">
+                    Контакты
+                </a>
+            </div>
+        </div>
+        <div class="chapter-wrap">
+            <div class="chapter">
+                <h4 class="chapter-title">
+                    Мы в социальных сетях
+                </h4>
+                <div class="chapter-info">
+                    Сайт разработан в inordic.ru
+                </div>
+                <div class="chapter-info">
+                    2018 Все права защищены
+                </div>
+                <div class="chapter-info">
+                    <a href="#" class="chapter-social icon-twitter">t</a>
+                    <a href="#" class="chapter-social icon-facebook">f</a>
+                    <a href="#" class="chapter-social icon-instagram">i</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+
 </div>
 <script src="js/script.js"></script>
 <script src="js/<?= $footer_config['script'] ?>"></script>
 </body>
+
 </html>
